4f474649e4a0892087351f2dc2e6c364e5df469c
Philippe Gibert <philippe.gibert@gmail.com>
2021-01-21 21:24:13 +0100

    Day 4

---
+++ day4/index.js
@@ -0,0 +1,31 @@
+import dirname from "es-dirname";
+import fs from "fs";
+import {resolve} from "path";
+
+/**
+ * Advent of code 2020
+ * Day3 : https://adventofcode.com/2020/day/3
+ * Input source : https://adventofcode.com/2020/day/3/input
+ * @author Philippe Gibert <philippe.gibert@gmail.com> aka giboow
+ */
+
+// Read file and create a array contains data
+const fileInput = fs.readFileSync(resolve(dirname(), 'input.txt'), 'utf-8');
+
+const keysTofind = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+let validCounter = 0;
+fileInput.split('\n\n').map(value => {
+    const keysArray = value.replaceAll('\n', ' ')
+        .split(' ')
+        .map(v => v.split(':')[0])
+        .filter(v => keysTofind.includes(v));
+    const unique = [...new Set(keysArray)];
+
+    if (unique.length == keysTofind.length) {
+        validCounter++;
+    }
+
+});
+
+console.log(validCounter);
+
