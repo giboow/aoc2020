08ae0235e4c9440e7c2cc795deaa0b5d45e73138
Philippe Gibert <philippe.gibert@gmail.com>
2021-01-21 23:14:03 +0100

    day3 part 2

--- day3/index.js
+++ day3/index.js
@@ -13,27 +13,38 @@ import {resolve} from "path";
 const fileInput = fs.readFileSync(resolve(dirname(), 'input.txt'), 'utf-8')
 const map = fileInput.split("\n").map(value => value.split(''));
 
-let pointer = 0;
-let treeCounter = 0;
-const mapWidth = map[0].length;
-map.forEach(value => {
+const func = (incCol, incRow) => {
+    let pointer = 0;
+    let treeCounter = 0;
+    const mapWidth = map[0].length;
+    map.forEach((value, idx) => {
+        if(idx % incRow !== 0) {
+           // console.log(value.join(''))
+            return;
+        }
 
-    // If the pointer is on a tree, inc the tree counter
-    if (value[pointer] === '#') {
-        treeCounter++;
-        value[pointer] = 'X';
-    } else {
-        value[pointer] = '0';
-    }
+        // If the pointer is on a tree, inc the tree counter
+        if (value[pointer] === '#') {
+            treeCounter++;
+        }
 
-    // Increment pointer,
-    // if the pointer is out of map width, restart a the good position of the next line
-    pointer +=3;
-    if (pointer >= mapWidth) {
-       pointer = pointer - mapWidth;
-    }
+        // Increment pointer,
+        // if the pointer is out of map width, restart a the good position of the next line
+        pointer += incCol;
+        if (pointer >= mapWidth) {
+            pointer = pointer - mapWidth;
+        }
 
-    console.log(value.join(''))
-});
+    });
+    console.log(incCol, incRow, treeCounter);
+    return treeCounter;
+};
+console.log(func(3, 1));
+console.log(
+    func(1, 1) *
+    func(3, 1) *
+    func(5, 1) *
+    func(7, 1) *
+    func(1, 2)
+);
 
-console.log(treeCounter);
