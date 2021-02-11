de1516167ab2e8da0de8d1bb43e2701582e8878c
Philippe Gibert <philippe.gibert@gmail.com>
2021-02-11 22:10:05 +0100

    Day 4 part 2

--- day4/index.js
+++ day4/index.js
@@ -27,5 +27,48 @@ fileInput.split('\n\n').map(value => {
 
 });
 
-console.log(validCounter);
+console.log("Part 1 : ", validCounter);
 
+
+// Part 2
+const specif = {
+    'byr': (val) => val >= 1920 && val <= 2002,
+    'iyr': (val) => val >= 2010 && val <= 2020,
+    'eyr': (val) => val >= 2020 && val <= 2030,
+    'hgt': (val) => {
+        const regex = /(?<mesure>\d.*)(?<unit>cm|in)/gm;
+        const rgxRes = regex.exec(val);
+        if (rgxRes != null) {
+            const {mesure, unit} = rgxRes.groups;
+            const mesureInt = parseInt(mesure, 10);
+            if (unit === 'cm') {
+                return mesureInt >= 150 && mesureInt <= 193;
+            } else if (unit === 'in') {
+                return mesureInt >= 59 && mesureInt <= 76;
+            }
+        }
+        return false;
+    },
+    'hcl': (val) => /^\#[0-9a-f]{6}$/gm.exec(val) !== null,
+    'ecl': (val) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(val),
+    'pid': (val) => /^(?=.{9}$)([0-9]*)$/gm.exec(val) !== null,
+    'cid': (val) => true,
+
+}
+let validCounter2 = 0;
+fileInput.split('\n\n').map(value => {
+    const keysArray = value.replaceAll('\n', ' ')
+        .split(' ')
+        .filter(v => {
+            const [key, val] = v.split(':');
+            return keysTofind.includes(key) && specif[key](val)
+        });
+    const unique = [...new Set(keysArray)];
+
+    if (unique.length == keysTofind.length) {
+        validCounter2++;
+    }
+
+});
+
+console.log("Part 2 : ", validCounter2);
