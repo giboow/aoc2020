c607478896571eee66828e6e8c79c54183ae9916
Philippe Gibert <philippe.gibert@gmail.com>
2021-02-19 22:57:07 +0100

    Day 8 part 2

--- day8/index.js
+++ day8/index.js
@@ -1,16 +1,16 @@
 import fs from "fs";
 import {resolve} from "path";
 import dirname from "es-dirname";
+import _ from 'lodash';
 
 const fileInput = fs.readFileSync(resolve(dirname(), 'input.txt'), 'utf-8');
-const lines = fileInput.split("\n").filter(value => value.length >0);
+const lines = fileInput.split("\n").filter(value => value.length > 0);
 
-const data = lines.map(value => {
-   const res = /^(?<inst>[a-z]{3}) (?<op>(\+|\-))(?<val>\d*)$/gm.exec(value);
+let data = lines.map(value => {
+    const res = /^(?<inst>[a-z]{3}) (?<op>(\+|\-))(?<val>\d*)$/gm.exec(value);
     const {inst, op, val} = res.groups;
-    return {inst, op, val : parseInt(val, 10), visit: 0};
+    return {inst, op, val: parseInt(val, 10), visit: 0};
 });
-
 
 
 let index = 0;
@@ -18,18 +18,17 @@ let d = data[index];
 let value = 0;
 let visit = 0;
 do {
-    const currentD = d;
     d.visit++;
     visit = d.visit;
     if (d.inst === 'nop') {
         index++;
-    } else if(d.inst === 'jmp') {
-        if(d.op === '+') {
+    } else if (d.inst === 'jmp') {
+        if (d.op === '+') {
             index += d.val;
         } else {
             index -= d.val;
         }
-    } else if(d.inst === "acc") {
+    } else if (d.inst === "acc") {
         if (d.op === '+') {
             value += d.val;
         } else {
@@ -38,7 +37,72 @@ do {
         index++;
     }
     d = data[index];
-    console.log(currentD, value);
-} while (visit <=1)
+} while (visit <= 1)
 
-console.log(value);
+console.log("Par 1 : ", value);
+
+
+// PART 2 - Brut force method, test all solutions
+
+const search = (dataArray) => {
+    let indexSearch = 0;
+    let d = dataArray[indexSearch];
+    let value = 0;
+    let visited = [];
+    do {
+        visited.push(indexSearch);
+
+        if (d.inst === 'nop') {
+            indexSearch++;
+        } else if (d.inst === 'jmp') {
+            if (d.val === 0) {
+                break;
+            }
+            if (d.op === '+') {
+                indexSearch += d.val;
+            } else {
+                indexSearch -= d.val;
+            }
+        } else if (d.inst === "acc") {
+            if (d.op === '+') {
+                value += d.val;
+            } else {
+                value -= d.val;
+            }
+            indexSearch++;
+        }
+        try {
+            d = dataArray[indexSearch];
+        } catch (e) {
+            consol.log(e);
+            d = null
+        }
+
+    } while (!visited.includes(indexSearch) && d != null)
+
+    if(indexSearch === dataArray.length) {
+        return value;
+    } else {
+        return null;
+    }
+}
+
+
+let data2 = lines.map(value => {
+    const res = /^(?<inst>[a-z]{3}) (?<op>(\+|\-))(?<val>\d*)$/gm.exec(value);
+    const {inst, op, val} = res.groups;
+    return {inst, op, val: parseInt(val, 10)};
+});
+
+for (const d in data2) {
+    if(['nop', 'jmp'].includes(data2[d].inst)) {
+        const dataToTest = _.cloneDeep(data2);
+
+        dataToTest[d].inst = dataToTest[d].inst  === 'nop' ? 'jmp' : 'nop';
+        const result = search(dataToTest);
+        if (result != null ) {
+            console.log("Par 2 : ", result);
+            break;
+        }
+    }
+}
